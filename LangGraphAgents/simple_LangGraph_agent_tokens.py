6bcd4cff11b262131f6046b14e88e15ba0bf2ec2
Amit Pratap <00amitpratap@gmail.com>
2025-02-13 19:37:02 +0530

    requirements.txt updated

---
+++ LangGraphAgents/simple_LangGraph_agent_tokens.py
@@ -0,0 +1,83 @@
+from dotenv import load_dotenv
+_ = load_dotenv()
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated
+import operator
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+import asyncio 
+
+tool = TavilySearchResults(max_results=4) #increased number of results
+from langgraph.checkpoint.aiosqlite import AsyncSqliteSaver
+
+memory = AsyncSqliteSaver.from_conn_string(":memory:")
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+
+class Agent:
+
+    def __init__(self, model, tools, checkpointer, system=""):
+        self.system = system
+        graph = StateGraph(AgentState)
+        graph.add_node("llm", self.call_openai)
+        graph.add_node("action", self.take_action)
+        graph.add_conditional_edges(
+            "llm",
+            self.exists_action,
+            {True: "action", False: END}
+        )
+        graph.add_edge("action", "llm")
+        graph.set_entry_point("llm")
+        self.graph = graph.compile(checkpointer = checkpointer)
+        self.tools = {t.name: t for t in tools}
+        self.model = model.bind_tools(tools)
+
+    def exists_action(self, state: AgentState):
+        result = state['messages'][-1]
+        return len(result.tool_calls) > 0
+
+    def call_openai(self, state: AgentState):
+        messages = state['messages']
+        if self.system:
+            messages = [SystemMessage(content=self.system)] + messages
+        message = self.model.invoke(messages)
+        return {'messages': [message]}
+
+    def take_action(self, state: AgentState):
+        tool_calls = state['messages'][-1].tool_calls
+        results = []
+        for t in tool_calls:
+#             print(f"Calling: {t}")
+            if not t['name'] in self.tools:      # check for bad tool name from LLM
+                print("\n ....bad tool name....")
+                result = "bad tool name, retry"  # instruct LLM to retry if bad
+            else:
+                result = self.tools[t['name']].invoke(t['args'])
+            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+#         print("Back to the model!")
+        return {'messages': results}
+
+prompt = """You are a smart research assistant. Use the search engine to look up information. \
+You are allowed to make multiple calls (either together or in sequence). \
+Only look up information when you are sure of what you want. \
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+"""
+
+# Note, the query was modified to produce more consistent results. 
+# Results may vary per run and over time as search information and models change.
+
+messages = [HumanMessage(content="What is the weather in SF?")]
+model = ChatOpenAI(model="gpt-4o")  # requires more advanced model
+abot = Agent(model, [tool], system=prompt, checkpointer=memory)
+thread = {"configurable": {"thread_id": "4"}}
+async for event in abot.graph.astream_events({"messages": messages}, thread, version="v1"):
+    kind = event["event"]
+    if kind == "on_chat_model_stream":
+        content = event["data"]["chunk"].content
+        if content:
+            # Empty content in the context of OpenAI means
+            # that the model is asking for a tool to be invoked.
+            # So we only print non-empty content
+            print(content, end="")
